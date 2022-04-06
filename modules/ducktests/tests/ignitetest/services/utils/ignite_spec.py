f3b291c0994fe064a3888e571676a18aafe769a8
Sergey Korotkov <serge.korotkov@gmail.com>
2022-04-06 16:14:27 +0700

    IGNITE-15169 Fix both jmx and census metrics export

--- modules/ducktests/tests/ignitetest/services/utils/ignite_spec.py
+++ modules/ducktests/tests/ignitetest/services/utils/ignite_spec.py
@@ -116,7 +116,6 @@ class IgniteSpec(metaclass=ABCMeta):
             default_jvm_opts = merge_jvm_settings(default_jvm_opts,
                                                   ["-Dcom.sun.management.jmxremote",
                                                    "-Dcom.sun.management.jmxremote.port=1098",
-                                                   "-Dcom.sun.management.jmxremote.rmi.port=1098",
                                                    "-Dcom.sun.management.jmxremote.local.only=false",
                                                    "-Dcom.sun.management.jmxremote.authenticate=false",
                                                    "-Dcom.sun.management.jmxremote.ssl=false"])
