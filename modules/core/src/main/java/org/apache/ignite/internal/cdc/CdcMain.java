d7e9596d65fe256efec847fd5965d3c2190f28ed
Sergey Korotkov <serge.korotkov@gmail.com>
2024-07-23 16:06:52 +0700

    IGNITE-22775 Add code smells

--- modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
@@ -294,6 +294,8 @@ public class CdcMain implements Runnable {
         }
 
         consumer = new WalRecordsConsumer<>(cdcCfg.getConsumer(), log);
+
+        System.err.println("Replace this use of System.err by a logger.");
     }
 
     /** Runs Change Data Capture. */
