e747beb5c9afdd9bb3ae4165400bb5b11da7de95
Sergey Korotkov <serge.korotkov@gmail.com>
2024-07-25 23:38:27 +0700

    IGNITE-22775 Fix code smells

--- modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
@@ -312,7 +312,7 @@ public class CdcMain implements Runnable {
             throw new IgniteException(e);
         }
 
-        System.err.println("Code smell in PR.");
+        log.error("Code smell in PR.");
     }
 
     /** Runs Change Data Capture application with possible exception. */
